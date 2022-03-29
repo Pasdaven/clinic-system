c4ee08b09fe8a7d05fbe73ad76a01f3277bafb1b
Pascal <a306090603@gmail.com>
2022-03-29 15:11:40 +0800

    edit database_tables.sql

--- Database_Tables.sql
+++ Database_Tables.sql
@@ -23,21 +23,6 @@ CREATE TABLE patient
   )
 comment '病人'; 
 
-CREATE TABLE patient_records
-  (
-     records_id   INT UNSIGNED PRIMARY KEY comment '看診紀錄編號',
-     case_id      INT UNSIGNED NOT NULL comment '病歷號碼',
-     CONSTRAINT case_id FOREIGN KEY(case_id) REFERENCES patient (case_id) ON UPDATE CASCADE ON DELETE CASCADE,
-     doc_id       INT UNSIGNED NOT NULL comment ' 醫生編號',
-     CONSTRAINT doc_id FOREIGN KEY(doc_id) REFERENCES doctor (doc_id),
-     date         DATE comment '看診日期',
-     disease_name VARCHAR(20) comment '疾病名稱',
-     med_days     INT UNSIGNED comment '用藥天數',
-     med_id       INT UNSIGNED comment '藥物編號',
-     CONSTRAINT med_id FOREIGN KEY(med_id) REFERENCES medicine (med_id)
-  )
-comment '病歷';  
-
 CREATE TABLE medicine
   (
      med_id   INT UNSIGNED PRIMARY KEY comment '藥品編號',
@@ -54,3 +39,18 @@ CREATE TABLE book
      email_address    VARCHAR(30) NOT NULL comment '病人信箱'
   )
 comment '掛號'; 
+
+CREATE TABLE patient_records
+  (
+     records_id   INT UNSIGNED PRIMARY KEY comment '看診紀錄編號',
+     case_id      INT UNSIGNED NOT NULL comment '病歷號碼',
+     CONSTRAINT case_id FOREIGN KEY(case_id) REFERENCES patient (case_id) ON UPDATE CASCADE ON DELETE CASCADE,
+     doc_id       INT UNSIGNED NOT NULL comment ' 醫生編號',
+     CONSTRAINT doc_id FOREIGN KEY(doc_id) REFERENCES doctor (doc_id),
+     date         DATE comment '看診日期',
+     disease_name VARCHAR(20) comment '疾病名稱',
+     med_days     INT UNSIGNED comment '用藥天數',
+     med_id       INT UNSIGNED comment '藥物編號',
+     CONSTRAINT med_id FOREIGN KEY(med_id) REFERENCES medicine (med_id)
+  )
+comment '病歷';  
