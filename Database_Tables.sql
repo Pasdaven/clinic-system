806d31b80ed0db5f2387775d5eafc844848d7d9a
ridemountainpig <lin0905580802@gmail.com>
2022-03-30 10:32:24 +0800

    edit Database_Tables.sql

--- Database_Tables.sql
+++ Database_Tables.sql
@@ -1,32 +1,33 @@
 CREATE TABLE doctor (
   doc_id INT UNSIGNED PRIMARY KEY auto_increment comment '醫生編號',
-  id INT UNSIGNED NOT NULL UNIQUE comment '身分證',
-  doc_name VARCHAR(5) comment '姓名',
+  id_num VARCHAR(10) NOT NULL UNIQUE comment '身分證',
+  doc_name VARCHAR(40) comment '姓名',
   sex VARCHAR(1) comment '性別',
   birth DATE comment '生日',
   phonenum INT UNSIGNED comment '手機號碼',
   consulation_time DATETIME comment '看診時間'
 ) comment '醫生';
 CREATE TABLE patient (
-  id INT UNSIGNED NOT NULL UNIQUE comment '身分證',
+  id_num VARCHAR(10) NOT NULL UNIQUE comment '身分證',
   case_id INT UNSIGNED PRIMARY KEY auto_increment comment '病歷號碼',
-  patient_name VARCHAR(5) comment '姓名',
+  patient_name VARCHAR(40) comment '姓名',
   sex VARCHAR(1) comment '性別',
   birth DATE comment '生日',
-  blod_type VARCHAR(1) comment '血型',
+  blood_type VARCHAR(1) comment '血型',
   phone_num INT UNSIGNED comment '手機號碼',
   allergy INT UNSIGNED comment '藥物過敏'
 ) comment '病人';
 CREATE TABLE medicine (
   med_id INT UNSIGNED PRIMARY KEY comment '藥品編號',
-  med_name VARCHAR(20) comment '藥品名稱'
+  med_name VARCHAR(40) comment '藥品名稱'
+  med_academic_name VARCHAR(40) comment '藥品學術名稱'
 ) comment '藥物';
 CREATE TABLE book (
   book_id INT UNSIGNED PRIMARY KEY comment '掛號編號',
   book_state INT UNSIGNED comment '等候狀態',
   consulation_time DATETIME comment '看診時間',
   doc_id INT UNSIGNED NOT NULL comment '醫生編號',
-  patient_id INT UNSIGNED NOT NULL comment '病人身分證',
+  id_num VARCHAR(40) NOT NULL comment '病人身分證',
   email_address VARCHAR(30) NOT NULL comment '病人信箱'
 ) comment '掛號';
 CREATE TABLE patient_records (
