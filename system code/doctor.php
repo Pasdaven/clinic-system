3326516fdcfd6cc0d0deefc0c10c50a347e32cc3
ridemountainpig <lin0905580802@gmail.com>
2022-04-15 00:11:08 +0800

    add doctor.php

---
+++ system code/doctor.php
@@ -0,0 +1,18 @@
+<?php
+// 載入db.php來連結資料庫
+require_once 'database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doctor</title>
+</head>
+<body>
+    <h1>病患資料查詢</h1>
+    <form>
+</body>
+</html>
