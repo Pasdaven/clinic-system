2ca869a60ed833bd7f9ad7a43a76d60369661ed1
ridemountainpig <lin0905580802@gmail.com>
2022-04-16 01:24:31 +0800

    edit doctor file

--- app/doctor/doctor.php
+++ app/doctor/doctor.php
@@ -1,3 +1,7 @@
+<?php
+require_once("../db_con.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,53 +10,139 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doctor</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 
 <body>
-    <h3>病患資料查詢</h3>
+    <h3>病患病歷查詢</h3>
+    <form method="POST" action="./doctor.php">
+        請選擇病例號碼：
+        <!-- <input type="text" value="請輸入病歷號碼" name="case_id" id="case_id"><br> -->
+        <input list="patient_case_id" name="case_id"><br>
+        <datalist id="patient_case_id">
+            <?php
+            $sql =  "SELECT * FROM `patient`";
+            $result = mysqli_query($link, $sql);
+            $datas = array();
 
-    病患病歷號碼：
-    <input type="text" value="請輸入病歷號碼" name="case_id" id="case_id"><br>
-    <input type="submit" value="查詢" name="searchBtn" id="searchBtn"><br>
+            while ($row = mysqli_fetch_assoc($result)) {
+                $datas[] = $row;
+            }
 
-    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" id="liveToast">
-        <div class="toast-body" id="toast-body"></div>
-    </div>
+            for ($i = 0; $i < count($datas); $i++) {
+                echo '<option value=' . $datas[$i]["case_id"] . '>';
+            }
+            ?>
+        </datalist>
+        <input type="submit" value="查詢" name="searchBtn" id="searchBtn"><br>
+    </form>
 
-    <script>
-        $(document).ready(function() {
-            var inputField = document.getElementById("case_id");
-            var table = "patient_records";
-            inputField.addEventListener("keyup", function(event) {
-                if (event.keyCode === 13) {
-                    event.preventDefault();
-                    document.getElementById("searchBtn").click();
+    <?php
+    if (isset($_POST['case_id'])) {
+        $case_id = trim($_POST["case_id"]);
+        $patient_datas = array();
+        $sql = "SELECT * FROM `patient_records` WHERE `case_id`='$case_id'";
+
+        $result = mysqli_query($link, $sql);
+
+        if ($result) {
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $patient_datas[] = $row;
                 }
-            });
 
-            $("#searchBtn").click(function() {
-                var patient_id = document.getElementById("case_id").value;
-                $.ajax({
-                    url: "./patient_search.php",
-                    method: "GET",
-                    data: {
-                        c: case_id,
-                        t: table
-                    },
-                    success: function(res) {
-                        $('.info').html('抓到資料囉!');
-                        $("#toast-body").html(res);
-                        var toastLive = document.getElementById("liveToast");
-                        var toast = new bootstrap.Toast(toastLive);
-                        toast.show();
-                    },
-                });
-            });
-        });
-    </script>
+                $sql = "SELECT * FROM `patient` WHERE `case_id` = '$case_id'";
+                $result = mysqli_query($link, $sql);
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '病歷號碼：' . $row['case_id'] . ' 病患名稱：' . $row['patient_name'] . '<br><br>';
+                }
+
+                for ($i = 0; $i < count($patient_datas); $i++) {
+                    echo '看診紀錄編號：' . $patient_datas[$i]['record_id'] . '<br>';
+                    if ($patient_datas[$i]['comment'] == NULL) {
+                        $patient_datas[$i]['comment'] = 'NONE';
+                    }
+                    echo '醫生編號：' . $patient_datas[$i]['doc_id'] . ' 看診日期：' . $patient_datas[$i]['consulation_date'] . ' 疾病名稱：' . $patient_datas[$i]['disease_name'] . ' 用藥天數：' . $patient_datas[$i]['med_days'] . ' 備註：' . $patient_datas[$i]['comment'] . '<br><br>';
+                }
+            } else {
+                while ($row = mysqli_fetch_array($result)) {
+                    echo '病歷編號：' . $row['case_id'] . ' 病患名稱：' . $row['patient_name'] . '<br><br>';
+                }
+                echo '尚無就診紀錄<br>';
+            }
+            mysqli_free_result($result);
+        }
+        // mysqli_close($link);
+    }
+    // if (!empty($result)) {
+    //     // 如果結果不為空，就利用print_r方法印出資料
+    //     echo count($patient_datas);
+    //     print_r($patient_datas);
+    // } else {
+    //     // 為空表示沒資料
+    //     echo "查無資料";
+    // }
+    ?>
+
+<h3>新增病歷資料</h3>
+    <form method="GET" action="./doctor.php">
+        病歷號碼:
+        <input list="patient_case_id" name="patient_rec[]">
+        <datalist id="patient_case_id">
+            <?php
+            $sql =  "SELECT * FROM `patient`";
+            $result = mysqli_query($link, $sql);
+            $datas = array();
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $datas[] = $row;
+            }
+
+            for ($i = 0; $i < count($datas); $i++) {
+                echo '<option value=' . $datas[$i]["case_id"] . '>';
+            }
+            ?>
+        </datalist>
+        醫生編號：
+        <input list="doc_id" name="patient_rec[]">
+        <datalist id="doc_id">
+            <?php
+            $sql =  "SELECT * FROM `doctor`";
+            $result = mysqli_query($link, $sql);
+            $datas = array();
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                $datas[] = $row;
+            }
+
+            for ($i = 0; $i < count($datas); $i++) {
+                echo '<option value=' . $datas[$i]["doc_id"] . '>';
+            }
+            ?>
+        </datalist>
+        看診日期：
+        <input type="date" id="date" name="patient_rec[]"><br><br>
+        疾病名稱：
+        <input type="text" id="disease_name" name="patient_rec[]">
+        用藥天數：
+        <input type="text" id="med_days" name="patient_rec[]">
+        備註：
+        <input type="text" id="comment" name="patient_rec[] value="NONE"><br><br>
+        <input type="submit" value="新增" id="sub_new_rec">
+    </form>
+    <?php
+    require_once("../db_con.php");
+    if (isset($_GET["patient_rec"])) {
+        $patient_rec = $_GET["patient_rec"];
+        // print_r($patient_rec);
+
+        $sql = "INSERT INTO `patient_records` VALUES (NULL, '$patient_rec[0]', '$patient_rec[1]', '$patient_rec[2]', '$patient_rec[3]', '$patient_rec[4]', '$patient_rec[5]')";
+        if (mysqli_query($link, $sql)) {
+            echo '病患病歷新增成功';
+        } else {
+            echo '病患病歷新增失敗';
+        }
+    }
+    ?>
 </body>
 
 </html>
