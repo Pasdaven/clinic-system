14966e0e0ce16481aec909df3607835f9bd121ad
ridemountainpig <lin0905580802@gmail.com>
2022-04-15 17:34:00 +0800

    edit db_con.php

--- app/db_con.php
+++ app/db_con.php
@@ -6,7 +6,6 @@ $dbname = 'Clinic_System';
 $link = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
 if($link){
     echo "資料庫連接成功</br>";
-    // echo "正確連接資料庫";
 }
 else {
     echo "資料庫連接失敗</br>" . mysqli_connect_error();
