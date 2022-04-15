f2fa6c697c9f5427b84116d712efe32fb1add39a
David Ho <davidho0403@gmail.com>
2022-04-16 02:34:18 +0800

    add book feature

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  醫生：
+  <input type="button" value="前往" onclick="location.href='app/doctor/doctor.php'">
+  <br><br>
+  掛號：
+  <input type="button" value="前往" onclick="location.href='app/book/book.php'">
+</body>
+</html>
