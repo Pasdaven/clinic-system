118fdb45fddeb6b1047abb93eb0165691034eb1f
ridemountainpig <lin0905580802@gmail.com>
2022-04-17 01:15:35 +0800

    edit, change add record by case_id to id_num

--- app/doctor/index.php
+++ app/doctor/index.php
@@ -120,23 +120,8 @@ require_once("../../database/db_con.php");
 
     <h3>新增病歷資料</h3>
     <form method="POST" action="./index.php">
-        病歷號碼:
-        <input list="patient_case_id" name="patient_rec[]">
-        <datalist id="patient_case_id">
-            <?php
-            $sql =  "SELECT * FROM `patient`";
-            $result = mysqli_query($link, $sql);
-            $datas = array();
-
-            while ($row = mysqli_fetch_assoc($result)) {
-                $datas[] = $row;
-            }
-
-            for ($i = 0; $i < count($datas); $i++) {
-                echo '<option value=' . $datas[$i]["case_id"] . '>';
-            }
-            ?>
-        </datalist>
+        病患身分證號碼:
+        <input type="text" name="patient_rec[]">
         醫生編號：
         <input list="doc_id" name="patient_rec[]">
         <datalist id="doc_id">
@@ -190,26 +175,42 @@ require_once("../../database/db_con.php");
     if (isset($_POST["patient_rec"])) {
         $patient_rec = $_POST["patient_rec"];
         // print_r($patient_rec);
-
-        // insert patient records
-        $sql = "INSERT INTO `patient_records` VALUES (NULL, '$patient_rec[0]', '$patient_rec[1]', '$patient_rec[2]', '$patient_rec[3]', '$patient_rec[4]', '$patient_rec[5]')";
-        mysqli_query($link, $sql);
-
-        // get patient record_id
-        $sql = "SELECT * FROM `patient_records`  ORDER BY record_id DESC LIMIT 0 , 1";
+        // patient data
+        $id_num = $patient_rec[0];
+        $sql = "SELECT * FROM `patient` WHERE `id_num` = '$id_num'";
         $result = mysqli_query($link, $sql);
-        while ($row = mysqli_fetch_assoc($result)) {
-            $record_id = $row['record_id'];
-        }
+        $patient = array();
+        if ($result) {
+            if (mysqli_num_rows($result) > 0) {
 
-        // insert patient medicine records
-        if (isset($_POST["med_list"])) {
-            $med_list = $_POST["med_list"];
-            for ($i = 0; $i < count($med_list); $i++) {
-                $sql = "INSERT INTO `med_list` VALUES ('$record_id', '$med_list[$i]')";
+                while ($row = mysqli_fetch_array($result)) {
+                    $patient[] = $row;
+                }
+                $case_id = $patient[0]['case_id'];
+                // insert patient records
+                $sql = "INSERT INTO `patient_records` VALUES (NULL, '$case_id', '$patient_rec[1]', '$patient_rec[2]', '$patient_rec[3]', '$patient_rec[4]', '$patient_rec[5]')";
                 mysqli_query($link, $sql);
+
+                // get patient record_id
+                $sql = "SELECT * FROM `patient_records`  ORDER BY record_id DESC LIMIT 0 , 1";
+                $result = mysqli_query($link, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $record_id = $row['record_id'];
+                }
+
+                // insert patient medicine records
+                if (isset($_POST["med_list"])) {
+                    $med_list = $_POST["med_list"];
+                    for ($i = 0; $i < count($med_list); $i++) {
+                        $sql = "INSERT INTO `med_list` VALUES ('$record_id', '$med_list[$i]')";
+                        mysqli_query($link, $sql);
+                    }
+                }
+            } else {
+                echo '無此患者資料';
             }
         }
+
         // if (mysqli_query($link, $sql)) {
         //     echo '病患病歷新增成功';
         // } else {
