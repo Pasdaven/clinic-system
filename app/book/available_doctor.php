b042e84c81275e965d3e19f3ef31b86c482668f7
David Ho <davidho0403@gmail.com>
2022-04-17 12:03:35 +0800

    change $week_day's representaion to number

--- app/book/available_doctor.php
+++ app/book/available_doctor.php
@@ -7,56 +7,30 @@ require_once("../../database/db_con.php");
 $week_day = "1";
 $time = "9";
 
-switch ($week_day) {
-  case 1:
-    $search_week_day = "Mon";
-    break;
-  case 2:
-    $search_week_day = "Tues";
-    break;
-  case 3:
-    $search_week_day = "Wed";
-    break;
-  case 4:
-    $search_week_day = "Thur";
-    break;
-  case 5:
-    $search_week_day = "Fri";
-    break;
-  case 6:
-    $search_week_day = "Sat";
-    break;
-  case 7:
-    $search_week_day = "Sun";
-    break;
-  default:
-    break;
-}
-
 switch ($time) {
   case 9:
   case 10:
   case 11:
-    $search_time = "morning";
+    $time_period = "morning";
     break;
   case 12:
   case 13:
   case 14:
   case 15:
-    $search_time = "evening";
+    $time_period = "evening";
     break;
   case 17:
   case 18:
   case 19:
   case 20:
-    $search_time = "noon";
+    $time_period = "noon";
     break;
   default:
-    $search_time = "wrong";
+    $time_period = "wrong";
     break;
 }
 
-$sql = "SELECT * FROM schedule WHERE week_day = '$search_week_day' && time_period = '$search_time'";
+$sql = "SELECT * FROM schedule WHERE week_day = '$week_day' && time_period = '$time_period'";
 $result = mysqli_query($link, $sql);
 $doctor_list = array();
 $doctor_name = array();
