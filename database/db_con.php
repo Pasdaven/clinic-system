cf713dc49fba13f945358f0aad8fa62de5179710
David Ho <davidho0403@gmail.com>
2022-04-18 02:30:27 +0800

    restore db_con.php

---
+++ database/db_con.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbuser = 'root';
+$dbpassword = '';
+$dbname = 'Clinic_System';
+$link = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
+if($link){
+    // echo "資料庫連接成功</br>";
+}
+else {
+    echo "資料庫連接失敗</br>" . mysqli_connect_error();
+}
+?>
