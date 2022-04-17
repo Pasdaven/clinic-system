77b5ffbc85a337de1ea2a9e9f3ce020814c6fa8d
David Ho <davidho0403@gmail.com>
2022-04-17 12:04:07 +0800

    hide db connect successfully's message

--- database/db_con.php
+++ database/db_con.php
@@ -5,7 +5,7 @@ $dbpassword = '';
 $dbname = 'Clinic_System';
 $link = mysqli_connect($host,$dbuser,$dbpassword,$dbname);
 if($link){
-    echo "資料庫連接成功</br>";
+    // echo "資料庫連接成功</br>";
 }
 else {
     echo "資料庫連接失敗</br>" . mysqli_connect_error();
