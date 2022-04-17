e942af488141bb413453f5526c703495aa2fabcc
David Ho <davidho0403@gmail.com>
2022-04-18 02:16:03 +0800

    Add core model class

--- model/model.php
+++ model/model.php
@@ -2,7 +2,7 @@
 
 class Model {
 
-  protected function getDB() {
+  private function getDB() {
     $host = 'localhost';
     $dbuser = 'root';
     $dbpassword = '';
@@ -12,6 +12,34 @@ class Model {
     return $link;
   }
 
-}
+  public function execute($sql) {
+    $result = mysqli_query($this->getDB(), $sql);
+    return $result;
+  }
 
-?>
+  public function getAll() {
+    $sql = "SELECT * FROM $this->table";
+    $list = $this->execute($sql);
+    while ($row = mysqli_fetch_array($list)) {
+      $result = $row;
+    }
+    return $result;
+  }
+
+  public function getSingleAttrAll($attr) {
+    $sql = "SELECT $attr FROM $this->table";
+    $list = $this->execute($sql);
+    while ($row = mysqli_fetch_array($list)) {
+      $result[] = $row;
+    }
+    return $result;
+  }
+  
+  public function getSingle($key) {
+    $sql = "SELECT * FROM $this->table WHERE $this->key_name = $key";
+    $result = $this->execute($sql);
+    $row = mysqli_fetch_array($result);
+    return $row;
+  }
+
+}
