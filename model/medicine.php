405703ea266523e1172304dc9a3ac9c72cf0bcf7
ridemountainpig <lin0905580802@gmail.com>
2022-04-18 16:07:06 +0800

    add insert & update function

---
+++ model/medicine.php
@@ -0,0 +1,36 @@
+<?php
+
+require("model.php");
+
+class Medicine extends Model {
+    protected $table = 'medicine';
+    protected $keyname = 'med_id';
+
+    //顯示藥品資料
+    public function showMedInfo($med_id) {
+        return $this->getSingle($this->table, $this->keyname, $med_id);
+    }
+
+    //新增藥品資料
+    public function insertMedInfo($med_id, $med_name, $med_academic_name, $med_effect) {
+        $sql = "INSERT INTO $this->table VALUES ('$med_id', '$med_name', '$med_academic_name', '$med_effect')";
+        if (!$this->execute($sql)) {
+            return "SQL error";
+        }
+    }
+
+    //更改藥品資料
+    // change place : 修改屬性, change text : 修改內容
+    public function updateMedInfo($med_id, $change_place, $change_text) {
+        $sql = "UPDATE $this->table SET $change_place = '$change_text' WHERE med_id = '$med_id'";
+        if ($change_place == "med_id") { // med_id is PK 不能修改
+            return "PK error";
+        } else {
+            if (!$this->execute($sql)) {
+                return "SQL error";
+            } else {
+                return true;
+            }
+        }
+    }
+}
