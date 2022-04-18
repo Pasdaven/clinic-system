9a038abde65b4fda7e3ae2a5a33497e6d36179b2
ridemountainpig <lin0905580802@gmail.com>
2022-04-18 16:06:55 +0800

    add insert & update function

--- model/Doctor.php
+++ model/Doctor.php
@@ -23,4 +23,27 @@ class Doctor extends Model {
   public function showDocInfo($doc_id) {
     return $this->getSingle($this->table, $this->key_name, $doc_id);
   }
+
+  //新增醫生資料
+  public function insertDocInfo($doc_id, $id_num, $doc_name, $sex, $birth, $phone_num, $doc_state) {
+    $sql = "INSERT INTO $this->table VALUES ('$doc_id', '$id_num', '$doc_name', '$sex', '$birth', '$phone_num', '$doc_state')";
+    if (!$this->execute($sql)) {
+      return "SQL error";
+    }
+  }
+
+  //修改醫生資料
+  // change place : 修改屬性, change text : 修改內容
+  public function updateDocInfo($doc_id, $change_place, $change_text) {
+    $sql = "UPDATE $this->table SET $change_place = '$change_text' WHERE doc_id = '$doc_id'";
+    if ($change_place == "doc_id") { // doc_id is PK 不能修改
+      return "PK error";
+    } else {
+      if ($this->execute($sql)) {
+        return "SQL error";
+      } else {
+        return true;
+      }
+    }
+  }
 }
