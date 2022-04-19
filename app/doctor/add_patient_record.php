5508923d1af78aae998110ac3e672df0ac9c317c
ridemountainpig <lin0905580802@gmail.com>
2022-04-19 22:06:57 +0800

    edit, change SQL code to function

--- app/doctor/add_patient_record.php
+++ app/doctor/add_patient_record.php
@@ -6,14 +6,8 @@
     <input list="doc_id" name="patient_rec[]">
     <datalist id="doc_id">
         <?php
-        $sql =  "SELECT * FROM doctor";
-        $result = mysqli_query($link, $sql);
-        $datas = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            $datas[] = $row;
-        }
-
-        foreach ($datas as $d) {
+        $doc_datas = $Doctor->getAllDocInfo();
+        foreach ($doc_datas as $d) {
             echo '<option value=' . $d["doc_id"] . '>';
         }
         ?>
@@ -28,14 +22,8 @@
     <input type="text" id="comment" name="patient_rec[] value=" NONE"><br><br>
     治療藥品：<br><br>
     <?php
-    $sql = "SELECT * FROM medicine";
-    $result = mysqli_query($link, $sql);
-    $med = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-        $med[] = $row;
-    }
-
-    foreach ($med as $i => $m) {
+    $med_datas = $Medicine->getAllMedInfo();
+    foreach ($med_datas as $i => $m) {
         echo '<input type="checkbox" name="med_list[]" value=' . $m['med_id'] . '>' . $m['med_id'] . $m['med_name'] . '</input>';
         if (($i + 1) % 5 == 0) {
             echo '<br>';
@@ -46,44 +34,19 @@
     <input type="submit" value="新增" id="sub_new_rec">
 </form>
 <?php
+
 if (isset($_POST["patient_rec"])) {
     $patient_rec = $_POST["patient_rec"];
-    // print_r($patient_rec);
-    // patient data
-    $id_num = $patient_rec[0];
-    $sql = "SELECT case_id FROM patient WHERE id_num = '$id_num'";
-    $result = mysqli_query($link, $sql);
-    $patient = array();
-    if (mysqli_num_rows($result)) {
-
-        $patient = mysqli_fetch_assoc($result);
-        $case_id = $patient['case_id'];
-        // insert patient records
-        $sql = "INSERT INTO patient_records VALUES (NULL, '$case_id', '$patient_rec[1]', '$patient_rec[2]', '$patient_rec[3]', '$patient_rec[4]', '$patient_rec[5]')";
-        mysqli_query($link, $sql);
-
-        // get patient record_id
-        $sql = "SELECT record_id FROM patient_records  ORDER BY record_id DESC LIMIT 0 , 1";
-        $result = mysqli_query($link, $sql);
-        $patient = mysqli_fetch_assoc($result);
-        $record_id = $patient['record_id'];
-
-        // insert patient medicine records
+    if ($Patient->patientExist($patient_rec[0])) {
+        $patient_data = $Patient->showpatInfo("$patient_rec[0]");
+        $Patient->addPatRec($patient_data['case_id'], $patient_rec[1], $patient_rec[2], $patient_rec[3], $patient_rec[4], $patient_rec[5]);
         if (isset($_POST["med_list"])) {
-            $med_list = $_POST["med_list"];
-            foreach ($med_list as $m_l) {
-                $sql = "INSERT INTO med_list VALUES ('$record_id', '$m_l')";
-                mysqli_query($link, $sql);
-            }
+            $patient_med = $_POST["med_list"];
+            $patient_show_rec = $Patient->showRecords($patient_rec[0]);
+            $Patient->addPatMed($patient_show_rec[count($patient_show_rec) - 1]['record_id'], $patient_med);
         }
     } else {
         echo '無此患者資料';
     }
-
-    // if (mysqli_query($link, $sql)) {
-    //     echo '病患病歷新增成功';
-    // } else {
-    //     echo '病患病歷新增失敗';
-    // }
 }
 ?>
--- app/patient_record_inquire.php
+++ app/patient_record_inquire.php
@@ -6,41 +6,41 @@
 </form>
 
 <?php
-require_once("../../model/patient.php");
-
 if (isset($_POST['id_num'])) {
-    $id_num = trim($_POST['id_num']);
-    // patient data
-    $patient = new Patient();
-    $patient->showpatInfo($id_num);
-    if ($patient->patientExist($id_num)) {
-        echo '病歷號碼：' . $patient->showpatInfo($id_num)['case_id'] . '<br>病患身分證號碼：' . $patient->showpatInfo($id_num)['id_num'] . '<br>病患名稱：' . $patient->showpatInfo($id_num)['patient_name'] . '<br><br>';
+    $id_num = $_POST['id_num'];
+    
+    if ($Patient->patientExist($id_num)) {
+        $patient_rec = $Patient->showpatInfo("$id_num");
+        echo '病歷號碼：' . $patient_rec['case_id'] . '<br>';
+        echo '病患身分證號碼：' . $patient_rec['id_num'] . '<br>';
+        echo '病患名稱：' . $patient_rec['patient_name'] . '<br><br>';
+        
         // patient allergy medicine
-        if (!$patient->medicineExist($id_num)) {
+        if (!$Patient->medicineExist($id_num)) {
             echo '無病患過敏藥物<br><br>';
         } else {
             echo '病患過敏藥物 : ';
-            foreach ($patient->showpatAlleMed($id_num) as $i => $a_d) {
+            foreach ($Patient->showpatAlleMed($id_num) as $i => $a_d) {
                 echo (string)$a_d['allergy_med_id'];
-                if ($i < $patient->medicineExist($id_num) - 1) {
+                if ($i < $Patient->medicineExist($id_num) - 1) {
                     echo ' , ';
                 }
             }
             echo '<br><br>';
         }
         // patient records
-        foreach ($patient->showRecords($id_num) as $p_d) {
+        foreach ($Patient->showRecords($id_num) as $p_d) {
             echo '看診紀錄編號：' . $p_d['record_id'] . '<br>';
             if ($p_d['comment'] == NULL) {
                 $p_d['comment'] = 'NONE';
             }
             echo '醫生編號：' . $p_d['doc_id'] . ' 看診日期：' . $p_d['consulation_date'] . ' 疾病名稱：' . $p_d['disease_name'] . ' 用藥天數：' . $p_d['med_days'] . ' 備註：' . $p_d['comment'] . '<br><br>';
             // medicine id 
-            if ($patient->showMedicines($p_d['record_id'])) {
+            if ($Patient->showMedicines($p_d['record_id'])) {
                 echo '使用藥品編號：';
-                foreach ($patient->showMedicines($p_d['record_id']) as $i => $m_l) {
+                foreach ($Patient->showMedicines($p_d['record_id']) as $i => $m_l) {
                     echo $m_l['med_id'];
-                    if ($i < count($patient->showMedicines($p_d['record_id'])) - 1) {
+                    if ($i < count($Patient->showMedicines($p_d['record_id'])) - 1) {
                         echo ' , ';
                     }
                 }
