8a55ccbc58b7411df21f7f0f4af582a024ab6595
ridemountainpig <lin0905580802@gmail.com>
2022-04-19 23:16:43 +0800

    edit, change SQL code to function

--- app/doctor/medicine_inquire.php
+++ app/doctor/medicine_inquire.php
@@ -7,16 +7,13 @@
 
 <?php
 if (isset($_POST['med_id'])) {
-    $med_id = trim($_POST["med_id"]);
-    $sql = "SELECT * FROM `medicine` WHERE `med_id`='$med_id'";
-    $result = mysqli_query($link, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        echo '藥品編號：' . $row['med_id'] . ' 藥品名稱：' . $row['med_name'] . ' 藥品學術名稱：' . $row['med_academic_name'] . ' 藥品作用：' . $row['med_effect'] . '<br><br>';
+    $med_id = $_POST["med_id"];
+    $result = $Medicine->showMedInfo($med_id);
+    
+    if ($result != NULL) {
+        echo '藥品編號：' . $result['med_id'] . ' 藥品名稱：' . $result['med_name'] . ' 藥品學術名稱：' . $result['med_academic_name'] . ' 藥品作用：' . $result['med_effect'] . '<br><br>';
     } else {
         echo '無此藥品資料<br>';
     }
-    mysqli_free_result($result);
 }
 ?>
