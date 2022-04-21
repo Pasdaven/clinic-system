fffcbff62446bc7a3b3faba789c19cce16ac3a43
ridemountainpig <lin0905580802@gmail.com>
2022-04-22 00:18:18 +0800

    edit, change to use fetch

--- app/doctor/doctor_data_inquire.php
+++ app/doctor/doctor_data_inquire.php
@@ -1,23 +1,30 @@
-<h3>醫生個人資料查詢</h3>
-<form method="POST" action="./index.php">
-    請輸入醫生身分證號碼：
-    <input type="text" name="doc_id_num">
-    <input type="submit" value="查詢" name="searchBtn" id="searchBtn"><br>
-</form>
 <?php
-if (isset($_POST["doc_id_num"])) {
-    $doc_id_num = $_POST["doc_id_num"];
-    if ($Doctor->doctorExist($doc_id_num)) {
-        $doctor_data = $Doctor->showDocInfo($doc_id_num);
-        echo '醫生編號：' . $doctor_data['doc_id'] . '<br>';
-        echo '醫生身分證號碼：' . $doctor_data['id_num'] . '<br>';
-        echo '醫生名稱：' . $doctor_data['doc_name'] . '<br>';
-        echo '性別：' . $doctor_data['sex'] . '<br>';
-        echo '生日：' . $doctor_data['birth'] . '<br>';
-        echo '手機號碼：' . $doctor_data['phone_num'] . '<br>';
-        echo '工作情況：' . $doctor_data['doc_state'] . '<br><br>';
-    } else {
-        echo '無此醫生資料<br>';
-    }
+require_once("../../model/Doctor.php");
+$Doctor = new Doctor();
+
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+
+if ($contentType === "application/json") {
+    $content = trim(file_get_contents("php://input"));
+
+    $decoded = json_decode($content, true);
+
+    $doc_id_num = $decoded["doc_id_num"];
+
+    $doctor_data = $Doctor->showDocInfo($doc_id_num);
+    
+    $decoded['doc_id'] = $doctor_data['doc_id'];
+    $decoded['id_num'] = $doctor_data['id_num'];
+    $decoded['doc_name'] = $doctor_data['doc_name'];
+    $decoded['sex'] = $doctor_data['sex'];
+    $decoded['birth'] = $doctor_data['birth'];
+    $decoded['phone_num'] = $doctor_data['phone_num'];
+    $decoded['doc_state'] = $doctor_data['doc_state'];
+
+    // $decoded['bar'] = "Hello World AGAIN!";    // Add some data to be returned.
+
+    $reply = json_encode($decoded);
 }
-?>
+
+header("Content-Type: application/json; charset=UTF-8");
+echo $reply;
