14ee937e2c839c9383f98680456c2f311d89741e
ridemountainpig <lin0905580802@gmail.com>
2022-04-22 01:43:30 +0800

    edit, change script example to fetch

--- json_example/script.js
+++ json_example/script.js
@@ -1,17 +1,18 @@
-let xmlhttp = new XMLHttpRequest();
-
-xmlhttp.onreadystatechange = function () {
-	if (this.readyState == 4 && this.status == 200) {
-		let data = JSON.parse(this.responseText);
-		changeHTML(data);
-	}
+function functionName() {
+    var url = 'core_link';
+    let data = {
+        controller: 'test url',
+        method: 'test method',
+        parameter: {
+            //parameter
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(res => console.log(res));
 }
-
-function changeHTML(data) {
-	document.getElementById("name").innerHTML=data[0];
-	document.getElementById("id").innerHTML=data[1];
-	document.getElementById("state").innerHTML=data[2];
-}
-
-xmlhttp.open("GET", "./api.php", true);
-xmlhttp.send();
