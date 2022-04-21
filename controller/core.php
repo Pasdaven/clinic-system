87a678990794860d8020bc456b8056c77cf1235d
Pascal <a306090603@gmail.com>
2022-04-22 02:25:25 +0800

    no parameter case

--- controller/core.php
+++ controller/core.php
@@ -4,8 +4,13 @@ $decoded = json_decode(file_get_contents('php://input'), true);
 
 require_once($decoded['controller'] . '.php');
 $obj = new $decoded['controller']();
-$string =  implode(',', $decoded['parameter']);
-$data = $obj->$decoded['method']($string);
+if ($decoded['parameter']) {
+    $string =  implode(',', $decoded['parameter']);
+    $data = $obj->$decoded['method']($string);
+} else {
+    $data = $obj->$decoded['method']();
+}
+
 
 $result = json_encode($data);
 
