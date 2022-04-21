21da1b2326348efab065b6533ed0761f9bad97e2
ridemountainpig <lin0905580802@gmail.com>
2022-04-22 01:14:25 +0800

    david ho is so handsome

--- app/doctor/doctor_data_inquire.js
+++ app/doctor/doctor_data_inquire.js
@@ -1,14 +1,19 @@
 function getDocData() {
     var id_num = document.getElementById("doc_id_num").value;
     var url = 'doctor_data_inquire.php';
+    let data = {
+        controller: 'test url',
+        method: 'test method',
+        parameter: {
+            doc_id_num : id_num
+        }
+    }
     fetch(url, {
         method: 'POST',
-        body: JSON.stringify({
-            doc_id_num: String(id_num)
-        }),
+        body: JSON.stringify(data),
         headers: {
             'Content-Type': 'application/json'
         }
     }).then(res => res.json())
-    .then(res => console.log(res));
+        .then(res => console.log(res));
 }
--- app/doctor/doctor_data_inquire.php
+++ app/doctor/doctor_data_inquire.php
@@ -6,25 +6,12 @@ $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) 
 
 if ($contentType === "application/json") {
     $content = trim(file_get_contents("php://input"));
-
     $decoded = json_decode($content, true);
-
-    $doc_id_num = $decoded["doc_id_num"];
-
-    $doctor_data = $Doctor->showDocInfo($doc_id_num);
     
-    $decoded['doc_id'] = $doctor_data['doc_id'];
-    $decoded['id_num'] = $doctor_data['id_num'];
-    $decoded['doc_name'] = $doctor_data['doc_name'];
-    $decoded['sex'] = $doctor_data['sex'];
-    $decoded['birth'] = $doctor_data['birth'];
-    $decoded['phone_num'] = $doctor_data['phone_num'];
-    $decoded['doc_state'] = $doctor_data['doc_state'];
-
-    // $decoded['bar'] = "Hello World AGAIN!";    // Add some data to be returned.
-
-    $reply = json_encode($decoded);
+    $doc_id_num = $decoded['parameter']['doc_id_num'];
+    $doctor_data = $Doctor->showDocInfo($doc_id_num);
+    $result = json_encode($doctor_data);
 }
 
 header("Content-Type: application/json; charset=UTF-8");
-echo $reply;
+echo $result;
