b2af7103fde0375512ea3db2830d64b2c189885f
ridemountainpig <lin0905580802@gmail.com>
2022-04-23 00:45:46 +0800

    add getDocList.js

---
+++ app/doctor/getDocList.js
@@ -0,0 +1,30 @@
+function getDocList() {
+    var url = '../../controller/core.php';
+    let data = {
+        controller: 'doctor',
+        method: 'getAllDocInfo',
+        parameter: {
+
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(res => {
+            const data = res;
+            console.log(data);
+            createDocList(data);
+        })
+}
+
+function createDocList(data) {
+    for (var i = 0; i < data.length; i++) {
+        let datalist = `<option value="${data[i]['doc_id']}">`;
+        console.log(datalist);
+        document.getElementById("doc_list").insertAdjacentHTML('afterbegin', datalist);
+    }
+}
