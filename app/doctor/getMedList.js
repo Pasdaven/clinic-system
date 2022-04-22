51e4fb79efa95263ea0db2a2475a636e76c3d540
ridemountainpig <lin0905580802@gmail.com>
2022-04-23 00:45:24 +0800

    add getMedList.js

---
+++ app/doctor/getMedList.js
@@ -0,0 +1,31 @@
+function getMedList() {
+    var url = '../../controller/core.php';
+    let data = {
+        controller: 'medicine',
+        method: 'getAllMedInfo',
+        parameter: {
+
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(res => {
+            const data = res;
+            console.log(data);
+            createMedList(data);
+        })
+}
+
+function createMedList(data) {
+    for (var i = data.length - 1; i >= 0; i--) {
+        let datalist = `<input type="checkbox" name="med_list[]" value="${data[i]['med_id']}">${data[i]['med_id'] + " " + data[i]['med_name']}</input>`;
+        console.log(datalist);
+        document.getElementById("med").insertAdjacentHTML('afterbegin', datalist);
+        
+    }
+}
