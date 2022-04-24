464d2f7b3b56ac4ea49a90dc4c069fa65ab743c7
David Ho <davidho0403@gmail.com>
2022-04-25 01:37:28 +0800

    add show book history feature

--- view/src/scripts/book/get_book_info.js
+++ view/src/scripts/book/get_book_info.js
@@ -12,11 +12,25 @@ $(document).ready(() => {
         url: '/clinic-system/controller/core.php',
         method: 'POST',
         data: json,
-        success: res => console.log(res)
+        success: res => displayInfo(res)
     });
 });
 
 let getUrl = () => {
     let param = new URLSearchParams(window.location.search);
     return param.get('url');
+}
+
+/*
+ echo '<h3>即時資訊</h3>';
+            echo '病患名稱：' . $row['patient_name'] . '<br>';
+            echo '等候狀態：' . $row['book_state'] . '<br>';
+            echo '看診時間：' . $row['consulation_time'] . '<br>';
+            echo '醫生：' . $doc_name . '<br>';
+*/
+
+let displayInfo = data => {
+    let info = document.getElementById('info');
+    let html = 'Name: ' + data.patient_name + '<br>' + 'state: ' + data.book_state + '<br>' + 'Doctor: ' + data.doc_name;
+    info.innerHTML = html;
 }
