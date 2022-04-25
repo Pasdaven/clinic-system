65d0ad47018d95fae2ef25844f71ef8062ace7e6
David Ho <davidho0403@gmail.com>
2022-04-25 16:53:51 +0800

    feat: add the choose doctor, time, room table
    將選擇醫生部分改為表格顯示，並列出早中晚時段以及一二診的醫生

--- view/src/scripts/book/doc_list.js
+++ view/src/scripts/book/doc_list.js
@@ -13,9 +13,22 @@ $(document).ready(() => {
 });
 
 let createDocList = data => {
+    let doc_tab = document.getElementById('doc_tab');
     for (i = 0; i < data.length; i++) {
         let obj = data[i];
-        let radio = '<input type="radio" name="doc_list" value="' + obj.doc_id + '" required>' + obj.doc_name;
-        document.getElementById("doc_list").insertAdjacentHTML('afterbegin', radio);
+        let row = document.createElement('tr');
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+        let td4 = document.createElement('td');
+        td1.innerHTML = '<input type="radio" name="doc_list" value="' + obj.doc_id + '" required>';
+        td2.innerHTML = obj.time_period;
+        td3.innerHTML = obj.room;
+        td4.innerHTML = obj.doc_name;
+        row.appendChild(td1);
+        row.appendChild(td2);
+        row.appendChild(td3);
+        row.appendChild(td4);
+        doc_tab.appendChild(row);
     }
 }
