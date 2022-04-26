26cae9808baadf92134115ac7a5af2656922907c
David Ho <davidho0403@gmail.com>
2022-04-26 14:44:51 +0800

    feat: queue method
    新增掛號號碼計算的method

--- model/book_mod.php
+++ model/book_mod.php
@@ -7,8 +7,8 @@ class Book_mod extends Model {
     protected $table = 'book';
     protected $key_name = 'book_id';
 
-    public function insert($patient_name, $id_num, $email_address, $schedule_id, $time) {
-        $sql = "INSERT INTO $this->table (patient_name, id_num, email_address, schedule_id, create_at) VALUES ('$patient_name', '$id_num', '$email_address', '$schedule_id', '$time')";
+    public function insert($patient_name, $id_num, $email_address, $schedule_id, $time, $date) {
+        $sql = "INSERT INTO $this->table (patient_name, id_num, email_address, schedule_id, create_at, create_date) VALUES ('$patient_name', '$id_num', '$email_address', '$schedule_id', '$time', '$date')";
         return $this->execute($sql);
     }
     public function select($id_num, $time) {
@@ -20,8 +20,16 @@ class Book_mod extends Model {
     public function selectUrl($book_url) {
         return $this->getSingle($this->table, 'book_url', $book_url);
     }
-    public function update($book_url, $book_id) {
+    public function updateUrl($book_url, $book_id) {
         $sql = "UPDATE $this->table SET book_url = '$book_url' WHERE book_id = $book_id";
+        return $this->execute($sql);
+    }
+    public function updateQueueNum($queue_num, $book_id) {
+        $sql = "UPDATE $this->table SET queue_num = '$queue_num' WHERE book_id = $book_id";
+        return $this->execute($sql);
+    }
+    public function selectSameTime($date, $schedule_id) {
+        $sql = "SELECT * FROM $this->table WHERE create_date = '$date' AND schedule_id = '$schedule_id'";
         return $this->execute($sql);
     }
 }
