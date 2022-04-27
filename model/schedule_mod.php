99de85d92541bd459ecbe9f0f3e426dbf9094ca8
David Ho <davidho0403@gmail.com>
2022-04-28 01:55:41 +0800

    feat: selectWeekAndTimePeriod()
    透過星期、時段進入db查找對應的醫生列

--- model/schedule_mod.php
+++ model/schedule_mod.php
@@ -15,4 +15,13 @@ class Schedule_mod extends Model {
         return $this->getMultiple($this->table, 'week_day', $week_day);
     }
 
+    public function selectWeekAndTimePeriod($week_day, $time_period) {
+        $sql = "SELECT * FROM $this->table WHERE week_day = '$week_day' AND time_period = '$time_period'";
+        $result = $this->execute($sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $list[] = $row;
+        }
+        return $list;
+    }
+
 }
