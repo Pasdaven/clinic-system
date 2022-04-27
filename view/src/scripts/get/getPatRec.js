81c58f840792e5c4af98727c151e5a3e5f32f66f
ridemountainpig <lin0905580802@gmail.com>
2022-04-28 02:01:55 +0800

    feat : search patient records insert html function

--- view/src/scripts/get/getPatRec.js
+++ view/src/scripts/get/getPatRec.js
@@ -1,6 +1,6 @@
 function getPatRec() {
     var id_num = document.getElementById("pat_id_num").value;
-    var url = '../../controller/core.php';
+    var url = '/clinic-system/controller/core.php';
     let data = {
         controller: 'patient_ctrl',
         method: 'showRecords',
@@ -18,5 +18,27 @@ function getPatRec() {
         .then(res => {
             const data = res;
             console.log(data);
+            if (document.getElementById("doctor_index") != null) {
+                for (var i = 0; i < data.length; i++) {
+                    let text = `
+                    <h4 id="${data[i]['record_id']}">Record Number : ${data[i]['record_id']}</h4>
+                    <div>
+                        Consulation Data : ${data[i]['consulation_date']}<br>
+                        Doctor ID : ${data[i]['doc_id']}<br>
+                        Disease Name : ${data[i]['disease_name']}<br>
+                        Medicine Days : ${data[i]['med_days']}<br>
+                    </div>
+                    <div id="${data[i]['record_id']}_med">
+                        Cure Medicine : 
+                    </div>
+                    `;
+                    document.querySelector('.scroll').insertAdjacentHTML('beforeend', text);
+
+                    let text1 = `
+                    <a class="list-group-item list-group-item-action" href="#${data[i]['record_id']}">${data[i]['record_id']}</a>
+                    `
+                    document.querySelector('.scroll_list').insertAdjacentHTML('beforeend', text1);
+                }
+            }
         })
 }
