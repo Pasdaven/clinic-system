acb7733c275f0dd8caa1cb587463c2729ce07d74
ridemountainpig <lin0905580802@gmail.com>
2022-04-29 09:47:28 +0800

    feat : insert show doctor info html & css & js

--- view/src/scripts/get/getDocData.js
+++ view/src/scripts/get/getDocData.js
@@ -1,6 +1,6 @@
 function getDocData() {
     var id_num = document.getElementById("doc_id_num").value;
-    var url = '../../controller/core.php';
+    var url = '/clinic-system/controller/core.php';
     let data = {
         controller: 'doctor_ctrl',
         method: 'showDocInfo',
@@ -15,9 +15,28 @@ function getDocData() {
             'Content-Type': 'application/json'
         }
     })
-    .then(res => res.json())
-    .then(res => {
-        const data = res;
-        console.log(data);
-    })
+        .then(res => res.json())
+        .then(res => {
+            const data = res;
+            console.log(data);
+            if (document.getElementById("searchDocInfo") != null) {
+                let text1 = `<div class="clean1" id="clean1"></div>`
+                document.querySelector('.box').insertAdjacentHTML('beforeend', text1);
+                let text = `
+                    <div class="p-5">
+                        <h4>Doctor Information</h4>
+                        <div class="px-2">
+                        Name: ${data['doc_name']}<br>
+                        ID : ${data['id_num']}<br>
+                        Doctor ID : ${data['doc_id']}<br>
+                        Doctor Work State : ${data['doc_state']}<br>
+                        Sex : ${data['sex']}<br>
+                        Birthday : ${data['birth']}<br>
+                        Phone Number : ${data['phone_num']}<br>
+                        </div>
+                    </div>
+                `;
+                document.querySelector('.clean1').insertAdjacentHTML('beforeend', text);
+            }
+        })
 }
