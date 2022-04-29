93afa87d5f1c2a0bf22cf656200d5e44285ef870
ridemountainpig <lin0905580802@gmail.com>
2022-04-29 09:57:51 +0800

    feat :  search medicine info show html & css & js

--- view/src/scripts/get/getMedInfo.js
+++ view/src/scripts/get/getMedInfo.js
@@ -18,5 +18,21 @@ function getMed() {
         .then(res => {
             const data = res;
             console.log(data);
+            if (document.getElementById("searchMedInfo") != null) {
+                let text1 = `<div class="clean1" id="clean1"></div>`
+                document.querySelector('.box').insertAdjacentHTML('beforeend', text1);
+                let text = `
+                    <div class="p-5">
+                        <h4>Medicine Information</h4>
+                        <div class="px-2">
+                        Medicine ID : ${data['med_id']}<br>
+                        Medicine Name : ${data['med_name']}<br>
+                        Medicine Academic Name : ${data['med_academic_name']}<br>
+                        Medicine Effect : ${data['med_effect']}<br>
+                        </div>
+                    </div>
+                `;
+                document.querySelector('.clean1').insertAdjacentHTML('beforeend', text);
+            }
         })
 }
