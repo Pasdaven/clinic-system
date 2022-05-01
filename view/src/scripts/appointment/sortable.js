82e2433ef8568e918c102bddeee4b7397ff58941
davidho0403 <davidho0403@gmail.com>
2022-05-02 01:41:50 +0800

    feat: show the toast notification
    在修改掛號狀態後，立即顯示修改成功的通知方塊

--- view/src/scripts/appointment/sortable.js
+++ view/src/scripts/appointment/sortable.js
@@ -21,8 +21,24 @@ $(document).ready(() => {
                 url: '/clinic-system/controller/core.php',
                 method: 'POST',
                 data: json,
-                success: res => console.log(res)
+                success: res => displayToast(res)
             });
         }
     });
 });
+
+let displayToast = res => {
+    let state_color;
+    if (res.state == "waiting") {
+        state_color = "text-primary";
+    } else if (res.state == "inProgress") {
+        state_color = "text-warning";
+    } else {
+        state_color = "text-success";
+    }
+    let html = `You have change patient <strong>${res.name}</strong>'s state to <a class="${state_color}" style="text-decoration:none;">${res.state}</a>.`;
+    $("#toast-message").html(html);
+    let toastLive = $("#liveToast");
+    let toast = new bootstrap.Toast(toastLive);
+    toast.show();
+}
