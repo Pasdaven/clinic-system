d9ce7fcd2b971a6cb35a37bbd98b03a57b1aaf03
davidho0403 <davidho0403@gmail.com>
2022-05-02 15:58:39 +0800

    feat: add time_period param to displayCurrentInfo
    增加選擇時段的篩選器，使用按鈕切換，並在頁面載入時預設為morning

--- view/src/scripts/appointment/getTodayAppointmentInfo.js
+++ view/src/scripts/appointment/getTodayAppointmentInfo.js
@@ -1,4 +1,8 @@
 $(document).ready(() => {
+    getTodayAppointmentInfo('morning');
+});
+
+let getTodayAppointmentInfo = (time_period) => {
     let data = {
         controller: 'appointment_ctrl',
         method: 'getTodayAppointmentInfo',
@@ -8,40 +12,213 @@ $(document).ready(() => {
         url: '/clinic-system/controller/core.php',
         method: 'POST',
         data: json,
-        success: res => displayCurrentInfo(res)
+        success: res => displayCurrentInfo(res, time_period)
     });
-});
+}
 
-let displayCurrentInfo = data => {
+let displayCurrentInfo = (data, time_period) => {
 
-    for (i = 0; i < data['waiting'].length; i++) {
-        let obj = data['waiting'][i];
-        let html = `
-            <div class="card my-2 p-3" id="${obj.appointment_id}">
-                Name: <h3>${obj.patient_name}</h3>
-                Queue: ${obj.queue_num}
-            </div>
-        `
-        $('#waiting').append(html);
+    $('#morning-btn').removeClass('btn-primary');
+    $('#evening-btn').removeClass('btn-primary');
+    $('#noon-btn').removeClass('btn-primary');
+    $('#morning-btn').addClass('btn-outline-dark');
+    $('#evening-btn').addClass('btn-outline-dark');
+    $('#noon-btn').addClass('btn-outline-dark');
+
+    $('#waiting').html('');
+    $('#inProgress').html('');
+    $('#finish').html('');
+
+    if (time_period == 'morning') {
+        $('#morning-btn').removeClass('btn-outline-dark');
+        $('#morning-btn').addClass('btn-primary');
+        for (i = 0; i < data['waiting'].length; i++) {
+            let obj = data['waiting'][i];
+            if (obj.time_period == 'morning') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#waiting').append(html);
+            }
+        }
+        for (i = 0; i < data['inProgress'].length; i++) {
+            let obj = data['inProgress'][i];
+            if (obj.time_period == 'morning') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#inProgress').append(html);
+            }
+        }
+        for (i = 0; i < data['finish'].length; i++) {
+            let obj = data['finish'][i];
+            if (obj.time_period == 'morning') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#finish').append(html);
+            }
+        }
     }
-    for (i = 0; i < data['inProgress'].length; i++) {
-        let obj = data['inProgress'][i];
-        let html = `
-            <div class="card my-2 p-3" id="${obj.appointment_id}">
-                Name: <h3>${obj.patient_name}</h3>
-                Queue: ${obj.queue_num}
-            </div>
-        `
-        $('#inProgress').append(html);
+    if (time_period == 'evening') {
+        $('#evening-btn').removeClass('btn-outline-dark');
+        $('#evening-btn').addClass('btn-primary');
+        for (i = 0; i < data['waiting'].length; i++) {
+            let obj = data['waiting'][i];
+            if (obj.time_period == 'evening') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#waiting').append(html);
+            }
+        }
+        for (i = 0; i < data['inProgress'].length; i++) {
+            let obj = data['inProgress'][i];
+            if (obj.time_period == 'evening') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#inProgress').append(html);
+            }
+        }
+        for (i = 0; i < data['finish'].length; i++) {
+            let obj = data['finish'][i];
+            if (obj.time_period == 'evening') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#finish').append(html);
+            }
+        }
     }
-    for (i = 0; i < data['finish'].length; i++) {
-        let obj = data['finish'][i];
-        let html = `
-            <div class="card my-2 p-3" id="${obj.appointment_id}">
-                Name: <h3>${obj.patient_name}</h3>
-                Queue: ${obj.queue_num}
-            </div>
-        `
-        $('#finish').append(html);
+    if (time_period == 'noon') {
+        $('#noon-btn').removeClass('btn-outline-dark');
+        $('#noon-btn').addClass('btn-primary');
+        for (i = 0; i < data['waiting'].length; i++) {
+            let obj = data['waiting'][i];
+            if (obj.time_period == 'noon') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#waiting').append(html);
+            }
+        }
+        for (i = 0; i < data['inProgress'].length; i++) {
+            let obj = data['inProgress'][i];
+            if (obj.time_period == 'noon') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#inProgress').append(html);
+            }
+        }
+        for (i = 0; i < data['finish'].length; i++) {
+            let obj = data['finish'][i];
+            if (obj.time_period == 'noon') {
+                let html = `
+                <div class="card my-2 p-3" id="${obj.appointment_id}">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            Name: <h3>${obj.patient_name}</h3>
+                        </div>
+                        <div class="col-lg-6 appointment-info">
+                            ID: ${obj.id_num}<br>
+                            Doctor: ${obj.doc_name}<br>
+                            Queue: ${obj.queue_num}<br>
+                        </div>
+                    </div>
+                </div>
+                `
+                $('#finish').append(html);
+            }
+        }
     }
 }
